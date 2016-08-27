27fa5e7d4b2fc6e797d46c8266d8736277cd2886
Kroo <kroo@dirvec.com>
2016-08-27 19:36:22 +0100

    Basic library outline

---
+++ src/bork.hpp
@@ -0,0 +1,35 @@
+/***************************************************************************************************
+*                                                                                                  *
+***************************************************************************************************/
+#ifndef _BORK_HPP_
+
+   #define _BORK_HPP_
+
+   #include "bork/ai.hpp"
+   #include "bork/algo.hpp"
+   #include "bork/assert.hpp"
+   #include "bork/async.hpp"
+   #include "bork/audio.hpp"
+   #include "bork/compute.hpp"
+   #include "bork/crypto.hpp"
+   #include "bork/debug.hpp"
+   #include "bork/filesys.hpp"
+   #include "bork/generic.hpp"
+   #include "bork/graphics.hpp"
+   #include "bork/image.hpp"
+   #include "bork/math.hpp"
+   #include "bork/memory.hpp"
+   #include "bork/meta.hpp"
+   #include "bork/network.hpp"
+   #include "bork/numeric.hpp"
+   #include "bork/parse.hpp"
+   #include "bork/physics.hpp"
+   #include "bork/platform.hpp"
+   #include "bork/random.hpp"
+   #include "bork/system.hpp"
+   #include "bork/target.hpp"
+   #include "bork/time.hpp"
+   #include "bork/ui.hpp"
+   #include "bork/video.hpp"
+
+#endif /* _BORK_HPP_ */
